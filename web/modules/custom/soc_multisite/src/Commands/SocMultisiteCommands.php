d5cf11fc93262c023e88c57be0ce071de8257221
cklein <christophe.klein@actency.fr>
2019-06-26 11:32:11 +0200

    feature/SOC-11 [WIP] Remove example code

--- web/modules/custom/soc_multisite/src/Commands/SocMultisiteCommands.php
+++ web/modules/custom/soc_multisite/src/Commands/SocMultisiteCommands.php
@@ -117,34 +117,4 @@ class SocMultisiteCommands extends DrushCommands {
     $this->logger()->success(dt('Achievement ' . $arg1 . ' unlocked.'));
   }
 
-  /**
-   * An example of the table output format.
-   *
-   * @param array $options An associative array of options whose values come from cli, aliases, config, etc.
-   *
-   * @field-labels
-   *   group: Group
-   *   token: Token
-   *   name: Name
-   * @default-fields group,token,name
-   *
-   * @command soc_multisite:token
-   * @aliases token
-   *
-   * @filter-default-field name
-   * @return \Consolidation\OutputFormatters\StructuredData\RowsOfFields
-   */
-  public function token($options = ['format' => 'table']) {
-    $all = \Drupal::token()->getInfo();
-    foreach ($all['tokens'] as $group => $tokens) {
-      foreach ($tokens as $key => $token) {
-        $rows[] = [
-          'group' => $group,
-          'token' => $key,
-          'name' => $token['name'],
-        ];
-      }
-    }
-    return new RowsOfFields($rows);
-  }
 }
