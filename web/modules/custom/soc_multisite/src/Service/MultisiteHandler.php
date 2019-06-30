0fa382fa5a5049c8102f7cdb5461540a6947b0c4
cklein <christophe.klein@actency.fr>
2019-06-30 11:44:54 +0200

    feature/SOC-11 Avoid to write duplicates in site.php + remove obsolete code

--- web/modules/custom/soc_multisite/src/Service/MultisiteHandler.php
+++ web/modules/custom/soc_multisite/src/Service/MultisiteHandler.php
@@ -2,63 +2,12 @@
 
 namespace Drupal\soc_multisite\Service;
 
-use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\EntityStorageException;
-use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Site\Settings;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
-use Symfony\Component\Process\Process;
-use Symfony\Component\Process\Exception\ProcessFailedException;
-use Symfony\Component\Finder\Finder;
 
 class MultisiteHandler {
-
-  private $envVariables = [
-    'HOME' => '/var/www',
-    'PATH' => '/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/var/www/.composer/vendor/bamarni/symfony-console-autocomplete/',
-  ];
-
-  /**
-   * Get enabled sites.
-   *
-   * @return array
-   */
-  public static function getEnabledSites() {
-    $app_root = \Drupal::root();
-    // Determine whether multi-site functionality is enabled.
-    $sitesFile = $app_root . '/sites/sites.php';
-    if (!file_exists($sitesFile)) {
-      return [];
-    }
-
-    require $app_root . '/sites/sites.php';
-    $list = [];
-    if (sizeof($sites)) {
-      foreach ($sites as $domain => $siteName) {
-        $list[$siteName] = $siteName;
-      }
-    }
-
-    return $list;
-  }
-
-  /**
-   * Get custom themes.
-   *
-   * @return array
-   */
-  public static function getCustomThemes() {
-    $app_root = \Drupal::root();
-    $finder = new Finder();
-    $directories = $finder->in($app_root . '/themes/custom')->depth(0)->directories();
-    $themes = [];
-    foreach ($directories as $theme) {
-      $themeName = $theme->getRelativePathname();
-      $themes[$themeName] = $themeName;
-    }
-    return $themes;
-  }
 
   /**
    * Create directories and files for a new Drupal site.
@@ -85,8 +34,11 @@ class MultisiteHandler {
         $host = str_replace(['^', '\\', '$'], [], reset($trusted_host_patterns));
         $fileSystem->appendToFile($sitesFile, PHP_EOL . '$sites["' . $host . '"] = "default";');
       }
-      // Add site to $sites variable.
-      $fileSystem->appendToFile($app_root . '/sites/sites.php', PHP_EOL . '$sites["' . $siteDomain . '"] = "' . $siteMachineName . '";');
+      require $app_root . '/sites/sites.php';
+      if (!array_key_exists($siteDomain, $sites)) {
+        // Add site to $sites variable.
+        $fileSystem->appendToFile($app_root . '/sites/sites.php', PHP_EOL . '$sites["' . $siteDomain . '"] = "' . $siteMachineName . '";');
+      }
       // Set $_site_name variable.
       $settingsPath = $app_root . '/sites/' . $siteMachineName . '/settings.php';
       $fileSystem->appendToFile($settingsPath, PHP_EOL . '$_site_name = "' . $siteMachineName . '";');
@@ -159,89 +111,6 @@ class MultisiteHandler {
         \Drupal::logger('soc_multisite')->error($e->getMessage());
       }
     }
-  }
-
-  /**
-   * @param $siteDomain
-   * @param $command
-   */
-  public function execConsoleCommand($siteDomain, $command) {
-    $app_root = \Drupal::root();
-    $process = new Process(
-      "../vendor/bin/drupal --uri=http://$siteDomain $command", $app_root, $this->envVariables, NULL, NULL
-    );
-    $process->run();
-
-    // executes after the command finishes
-    if (!$process->isSuccessful()) {
-      throw new ProcessFailedException($process);
-    }
-  }
-
-  /**
-   * @param $task
-   */
-  public function execPhingTask($task) {
-    $app_root = \Drupal::root();
-    $process = new Process(
-      "./vendor/bin/phing $task", $app_root . '/..', $this->envVariables, NULL, NULL
-    );
-    $process->run();
-
-    // executes after the command finishes
-    if (!$process->isSuccessful()) {
-      throw new ProcessFailedException($process);
-    }
-  }
-
-  /**
-   * @param $siteName
-   * @param $siteDomain
-   */
-  public function siteInstall($siteName, $siteDomain) {
-    $command = '--site-name="' . $siteName . '" site:install standard --force --no-interaction --langcode="fr"';
-    $this->execConsoleCommand($siteDomain, $command);
-  }
-
-  /**
-   * @param $siteDomain
-   */
-  public function cacheRebuildAll($siteDomain) {
-    $command = 'cache:rebuild all';
-    $this->execConsoleCommand($siteDomain, $command);
-  }
-
-  /**
-   * @param $siteDomain
-   */
-  public function importConfigurations($siteDomain) {
-    // Set UUID.
-    $command = 'config:override system.site uuid d74caedc-cdeb-485d-9401-1d3c898a1a1c';
-    $this->execConsoleCommand($siteDomain, $command);
-    // Delete Shortcut Set.
-    $command = 'entity:delete shortcut_set default';
-    $this->execConsoleCommand($siteDomain, $command);
-    // Import configurations.
-    $command = 'config:import --directory=/var/www/html/config/drupal/sync';
-    $this->execConsoleCommand($siteDomain, $command);
-    // Clear cache.
-    $this->cacheRebuildAll($siteDomain);
-  }
-
-  /**
-   * @param $siteDomain
-   * @param $sourceTheme
-   * @param $targetTheme
-   */
-  public function cloneTheme($siteDomain, $sourceTheme, $targetTheme) {
-    // Clone existing theme.
-    $task = "theme:clone -Dsource=$sourceTheme -Dtarget=$targetTheme";
-    $this->execPhingTask($task);
-    // Clear cache.
-    $this->cacheRebuildAll($siteDomain);
-    // Enable new theme.
-    $command = "theme:install $targetTheme --set-default";
-    $this->execConsoleCommand($siteDomain, $command);
   }
 
 }
