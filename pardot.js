bb73df631f7b41ab9e3fe1a3ff96a324fbc234e2
Guillaume <guicom.actency@gmail.com>
2019-10-18 16:30:28 +0200

    Add css & js

---
+++ pardot.js
@@ -0,0 +1,23 @@
+(function(){
+  $(document).ready(function() {
+    $('input').each(function () {
+      $(this).on('blur input', function() {
+        if ($(this).val()) {
+          $(this).parent().addClass('filled');
+        }
+        else {
+          $(this).parent().removeClass('filled');
+        }
+      });
+    });
+
+    $('.pd-checkbox').each(function () {
+      var input = $(this).find('input');
+      $(this).prepend(input);
+    });
+  });
+  $(window).on('load', function() {
+    $('body').show();
+  });
+})();
+
