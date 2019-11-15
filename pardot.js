8288a03dae48b348093b13efda649b6e14e57508
hesslinger <hugues.esslinger@actency.fr>
2019-11-15 12:50:39 +0000

    SOCSOB-362 Update focused

--- pardot.js
+++ pardot.js
@@ -9,6 +9,14 @@
           $(this).parent().removeClass('filled');
         }
       });
+      $(this).focus(function() {
+        $(this).parent().addClass('focused');
+      });
+      $(this).focus(function(){
+        $(this).parent().addClass('focused');
+      }).blur(function(){
+        $(this).parent().removeClass('focused');
+      });
     });
 
     $('.pd-checkbox').each(function () {
