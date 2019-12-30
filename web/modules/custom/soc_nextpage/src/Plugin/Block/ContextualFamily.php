d9d13cfb19280fb674861ef57c9c46732c3e1c90
Guillaume <guicom.actency@gmail.com>
2019-12-30 11:37:13 +0100

    #436 - Fix pager + block content + contextual block

--- web/modules/custom/soc_nextpage/src/Plugin/Block/ContextualFamily.php
+++ web/modules/custom/soc_nextpage/src/Plugin/Block/ContextualFamily.php
@@ -25,20 +25,28 @@ class ContextualFamily extends BlockBase {
     if (\Drupal::routeMatch()->getRouteName() == 'entity.taxonomy_term.canonical') {
       // load the term entity and get the data from there
       $term = \Drupal::routeMatch()->getParameter('taxonomy_term');
-      // Add vocabulary ono body class
+      // Get term ID.
       $tid = $term->id();
+      // Get vocabulary ID.
       $vid = $term->bundle();
-      $terms =\Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vid, $tid);
+      // Get tree of this term, only 1 level
+      $terms =\Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vid, $tid, 1);
 
+      // If no child, get sister
       if (empty($terms)) {
+        // Get parent of term
         $parent = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadParents($tid);
         $parent = reset($parent);
-        $terms =\Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vid, $parent->id());
+        // Get tree of this term, only 1 level
+        $terms =\Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($vid, $parent->id(), 1);
       }
 
+      // Build list
+      // We don't use item_list them as the generated markup is too specific
       $output = '<ul>';
       foreach($terms as $t) {
         $text = $t->name;
+        // If not current term, display as link
         if ($t->tid != $tid) {
           $url = Url::fromRoute('entity.taxonomy_term.canonical', ['taxonomy_term' => $t->tid]);
           $link = Link::fromTextAndUrl($text, $url);
