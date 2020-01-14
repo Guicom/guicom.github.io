74cd1e954379e07b0a9c8e98a34cbe8c0ac324e8
Guillaume <guicom.actency@gmail.com>
2020-01-14 14:56:30 +0100

    #535 - Top Navigation

--- config/drupal/example.settings.local.php
+++ config/drupal/example.settings.local.php
@@ -172,3 +172,8 @@ $settings['redis.connection']['host'] = 'redis';
  */
 $config['config_split.config_split.dev']['status'] = TRUE;
 $config['config_split.config_split.dev']['folder'] = "../config/drupal/dev";
+
+/**
+ * Drupal 8 MegaMenu Deploy
+ */
+$settings['we_megamenu_deploy_content'] = $app_ground . '/content/we_megamenu_content';
