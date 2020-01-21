c9d8ee42d0a2c63ac5f12363a80fcb2e32349e4e
Sebastien cunin <sebastien.cunin@gmail.com>
2020-01-21 11:55:05 +0100

    feature/SOCSOB-461: Add the link render array for the external link with/without "open new window" option.

--- web/modules/custom/soc_core/src/Plugin/ExtraField/Display/ButtonDownloadResourceDisplay.php
+++ web/modules/custom/soc_core/src/Plugin/ExtraField/Display/ButtonDownloadResourceDisplay.php
@@ -3,6 +3,8 @@
 namespace Drupal\soc_core\Plugin\ExtraField\Display;
 
 use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Url;
 use Drupal\extra_field\Plugin\ExtraFieldDisplayBase;
 
 
@@ -19,13 +21,48 @@ use Drupal\extra_field\Plugin\ExtraFieldDisplayBase;
  */
 class ButtonDownloadResourceDisplay extends ExtraFieldDisplayBase {
 
+  use StringTranslationTrait;
+
   public function view(ContentEntityInterface $entity) {
-    $output = [
-      '#type' => 'html_tag',
-      '#value' => 'ButtonDownloadResourceDisplay',
-      '#tag' => 'h1',
+
+
+    $url = Url::fromRoute('<current>');
+
+
+    $label = '';
+    $attributes = [];
+    if ($entity->get('field_res_downloadable')
+        ->getValue()[0]['value'] === '0') {
+      // je pose un lien classique sur la page.
+      $label = "Lien classique";
+      // field field_open_link_in_new_window.
+      if ($entity->get('field_open_link_in_new_window')
+          ->getValue()[0]['value'] === '1') {
+        $attributes = ['target' => '_blank'];
+      }
+      if ($entity->get('field_res_link_url')->first()->getValue()) {
+        $url = Url::fromUri($entity->get('field_res_link_url')
+          ->first()
+          ->getValue()['value']);
+      }
+
+
+    }
+    else {
+      // je pose un lien de téléchargement.
+      $label = "Lien download";
+
+      var_dump($entity->get('field_res_remote_file_url')->getValue());
+      // @todo: chercher le field correspondant.
+    }
+
+    $build['examples_link'] = [
+      '#title' => $label,
+      '#type' => 'link',
+      '#url' => $url,
+      '#attributes' => $attributes,
     ];
-    return $output;
+    return $build;
   }
 
 
