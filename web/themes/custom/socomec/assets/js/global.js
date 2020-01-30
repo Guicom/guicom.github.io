f9cfbff64977de94c429b25944d1a299c92c58ce
www-data <www-data@05cfa2d38e1e>
2020-01-30 16:39:00 +0000

    Script smooth scroll with offset

--- web/themes/custom/socomec/assets/js/global.js
+++ web/themes/custom/socomec/assets/js/global.js
@@ -203,4 +203,27 @@
     }
   };
 
+  /**
+   * Smooth scroll for anchor
+   */
+  Drupal.behaviors.socomec_smooth_anchor_scrolling = {
+    attach: function (context, settings) {
+      $(function() {
+        $('a[href*="#"]:not([href="#"])').click(function() {
+          var offset = -200;
+          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+              $('html, body').animate({
+                scrollTop: target.offset().top + offset
+              }, 1000);
+              return false;
+            }
+          }
+        });
+      });
+    }
+  };
+
 })(jQuery, Drupal);
