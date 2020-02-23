7e3434663aa41dd09aad1cab541fabc785d35fbf
Guillaume <guicom.actency@gmail.com>
2020-02-23 20:32:51 +0100

    #898 - Add icon field for menu taxonomy

---
+++ web/modules/custom/soc_nextpage/src/TwigExtension/NextpageTwig.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\soc_nextpage\TwigExtension;
+
+
+use Drupal\file\Entity\File;
+use Drupal\media\Entity\Media;
+use Drupal\taxonomy\Entity\Term;
+
+class NextpageTwig extends \Twig_Extension {
+
+  /**
+   * Generates a list of all Twig filters that this extension defines.
+   */
+  public function getFilters() {
+    return [
+      new \Twig_SimpleFilter('getfield', array($this, 'getFieldData')),
+      new \Twig_SimpleFilter('getMenuIcon', array($this, 'getMenuIcon')),
+    ];
+  }
+
+  /**
+   * Gets a unique identifier for this Twig extension.
+   */
+  public function getName() {
+    return 'NextpageTwig.twig_extension';
+  }
+
+  /**
+   * Get json field content.
+   */
+  public static function getFieldData($string, $extid) {
+    // ['Main incoming load break', 'Distribution load break','Machine control','Local safety load break']
+    $json_value = json_decode($string[0]["#context"]["value"]);
+    $data = NULL;
+    if (isset($json_value->Marketing->value->{$extid})) {
+      $data = $json_value->Marketing->value->{$extid}->value;
+    }
+    return $data;
+  }
+
+  /**
+   * Get menu item icon.
+   */
+  public static function getMenuIcon($string) {
+    // Get storage attributes.
+    $storage = $string->storage();
+    // Set return string.
+    $url = NULL;
+    if ($storage["data-level"]->value() === 1) {
+      // Get TID
+      $uuid = $storage["data-id"]->value();
+      $element = explode(".", $uuid);
+      $tid = end($element);
+      // Load term.
+      $term =  Term::load($tid);
+      $icon = $term->get('field_family_menu_icon')->getValue();
+      // Load Media.
+      $media = Media::load($icon[0]["target_id"]);
+      $media_item = $media->get('field_media_image')->getValue();
+      // Load file and get url
+      $file = File::load($media_item[0]["target_id"]);
+      $url = $file->createFileUrl();
+    }
+    return $url;
+  }
+}
