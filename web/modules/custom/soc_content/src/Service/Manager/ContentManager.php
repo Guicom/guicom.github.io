58f6df0581e422ff1a327049daa71febb89aa51f
Christophe Klein <christophe.klein.67@gmail.com>
2020-03-01 12:33:05 +0100

    WIP: Creation of a content management framework.

---
+++ web/modules/custom/soc_content/src/Service/Manager/ContentManager.php
@@ -0,0 +1,107 @@
+<?php
+
+
+namespace Drupal\soc_content\Service\Manager;
+
+
+use Drupal\Core\Entity\EntityRepository;
+use Drupal\Core\Entity\EntityStorageException;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\taxonomy\Entity\Term;
+
+class ContentManager {
+
+  /**
+   * The entity repository.
+   *
+   * @var \Drupal\Core\Entity\EntityRepository
+   */
+  protected $entityRepository;
+
+  /**
+   * The soc_content logging channel.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelInterface
+   */
+  protected $logger;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityRepository $entity_repository
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $channel_factory
+   */
+  public function __construct(EntityRepository $entity_repository,
+                              LoggerChannelFactoryInterface $channel_factory) {
+    $this->entityRepository = $entity_repository;
+    $this->logger = $channel_factory->get('soc_content');
+  }
+
+  /**
+   * @param string $type
+   * @param string $uuid
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|null
+   */
+  public function getEntityByUuid(string $type, string $uuid) {
+    try {
+      $entity = $this->entityRepository->loadEntityByUuid($type, $uuid);
+      if ($entity->getEntityType() === $type) {
+        return $entity;
+      }
+    } catch (\Exception $e) {
+      $this->logger->error($e->getMessage());
+    }
+    return NULL;
+  }
+
+  /**
+   * @param string $uuid
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|null
+   */
+  protected function getNodeByUuid(string $uuid) {
+    return $this->getEntityByUuid('node', $uuid);
+  }
+
+  /**
+   * @param string $uuid
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|null
+   */
+  protected function getTermByUuid(string $uuid) {
+    return $this->getEntityByUuid('taxonomy_term', $uuid);
+  }
+
+  /**
+   * @param string $uuid
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|null
+   */
+  protected function getMenuItemByUuid(string $uuid) {
+    return $this->getEntityByUuid('menu_link_content', $uuid);
+  }
+
+  /**
+   * @param string $uuid
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|null
+   */
+  public function getBlockContentByUuid(string $uuid) {
+    return $this->getEntityByUuid('block_content', $uuid);
+  }
+
+  /**
+   * @param $data
+   */
+  public function createTerm($data) {
+    $new_term = Term::create($data);
+    $new_term->enforceIsNew();
+    try {
+      $new_term->save();
+    } catch (EntityStorageException $e) {
+      $this->logger->error($e->getMessage());
+    }
+  }
+
+}
