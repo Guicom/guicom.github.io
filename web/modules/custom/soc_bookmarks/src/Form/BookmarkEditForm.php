4f1bd9823c2142370cd8855588456dfe86a2af6a
hesslinger <hugues.esslinger@actency.fr>
2020-03-26 09:42:41 +0000

    SOCSOB-555 Fix sonarqube duplicate

--- web/modules/custom/soc_bookmarks/src/Form/BookmarkEditForm.php
+++ web/modules/custom/soc_bookmarks/src/Form/BookmarkEditForm.php
@@ -2,24 +2,24 @@
 
 namespace Drupal\soc_bookmarks\Form;
 
-use Drupal\Core\Ajax\AjaxResponse;
-use Drupal\Core\Ajax\RemoveCommand;
-use Drupal\Core\Ajax\PrependCommand;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\node\Entity\Node;
 use Drupal\soc_core\Service\MediaApi;
 use Drupal\soc_bookmarks\Service\Manager\BookmarkManager;
+use \Drupal\soc_content_list\Service\Manager\ContentListFormManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Url;
-use Drupal\Core\Link;
 
 
 class BookmarkEditForm extends FormBase {
 
   /** @var \Drupal\soc_bookmarks\Service\Manager\bookmarkManager $bookmarkManager */
   private $bookmarkManager;
+
+  /** @var \Drupal\soc_content_list\Service\Manager\ContentListFormManager $contentListFormManager */
+  private $contentListFormManager;
 
   /**
    * The Messenger service.
@@ -36,18 +36,21 @@ class BookmarkEditForm extends FormBase {
   protected $mediaApi;
 
   /**
-   * WishlistController constructor.
+   * BookmarkEditForm constructor.
    *
    * @param \Drupal\soc_bookmarks\Service\Manager\BookmarkManager $bookmarkManager
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
    * @param \Drupal\soc_core\Service\MediaApi $mediaApi
+   * @param \Drupal\soc_content_list\Service\Manager\ContentListFormManager $contentListFormManager
    */
   public function __construct(BookmarkManager $bookmarkManager,
                               MessengerInterface $messenger,
-                              MediaApi $mediaApi) {
+                              MediaApi $mediaApi,
+                              ContentListFormManager $contentListFormManager) {
     $this->bookmarkManager = $bookmarkManager;
     $this->messenger = $messenger;
     $this->mediaApi = $mediaApi;
+    $this->contentListFormManager = $contentListFormManager;
   }
 
   /**
@@ -57,7 +60,8 @@ class BookmarkEditForm extends FormBase {
     return new static(
       $container->get('soc_bookmarks.bookmark_manager'),
       $container->get('messenger'),
-      $container->get('soc_core.media_api')
+      $container->get('soc_core.media_api'),
+      $container->get('soc_content_list.content_list_form_manager')
     );
   }
 
@@ -88,11 +92,7 @@ class BookmarkEditForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form['#theme'] = 'soc_bookmarks_custom_form';
-    if (empty($this->bookmarkManager)) {
-      $this->bookmarkManager = \Drupal::service('soc_bookmarks.bookmark_manager');
-    }
     $items = $this->bookmarkManager->loadSavedItems();
-
 
     // Form wrapper for AJAX
     $form['wrapper_bookmark'] = [
@@ -163,7 +163,6 @@ class BookmarkEditForm extends FormBase {
         '#value' => $i
       ];
 
-      $confirmRemoveClass = 'confirm-remove';
       for ($i = 0; $i < 2; ++$i) {
         // Update quantity
         if (sizeof($items)) {
@@ -182,29 +181,10 @@ class BookmarkEditForm extends FormBase {
               'progress' => ['type' => 'none'],
             ],
           ];
-          $form['actions']['others'][$i]['remove'][$i]['#attributes']['class'][] = $confirmRemoveClass;
+          $form['actions']['others'][$i]['remove'][$i]['#attributes']['class'][] = 'confirm-remove';
         }
       }
-
-      $form['#attached']['library'][] = 'soc_content_list/list-actions';
-
-      // Confirm before deleting
-      $form['#attached']['library'][] = 'core/drupal.ajax';
-      $form['#attached']['library'][] = 'core/drupal.dialog.ajax';
-      $form['#attached']['library'][] = 'soc_content_list/ajax-confirm';
-      $form['#attached']['drupalSettings']['ajaxConfirm'][$confirmRemoveClass] = [
-        'title' => $this->t('Confirm element removal'),
-        'text' => $this->t('Are you sure that you want to remove this element?'),
-        'buttons' => [
-          'button_confirm' => ['text' => $this->t('Yes')],
-          'button_reject' => ['text' => $this->t('No')],
-        ]
-      ];
-
-      $form['#attached']['library'][] = 'soc_content_list/list-datatable';
-      $form['#attached']['drupalSettings']['socListDatatable'] = [
-        'searchPlaceholder' => $this->t('Search, filter...'),
-      ];
+      $this->contentListFormManager->attached($form, 'confirm-remove');
     }
     else {
       $form['no_result'] = [
@@ -224,28 +204,7 @@ class BookmarkEditForm extends FormBase {
    *
    * @return array
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-  }
-
-  /**
-   * @param $input
-   *
-   * @return array
-   */
-  public static function customSelectedItems($input) {
-    $selectItems = [];
-    if (!empty($input) && is_array($input)) {
-      foreach ($input as $keyItem => $item) {
-        if (substr($keyItem, 0, 16) === "bookmark_action_") {
-          $keys = array_keys($item);
-          if (!empty($item[$keys[0]])) {
-            $selectItems[$item[$keys[0]]] = $item[$keys[0]];
-          }
-        }
-      }
-    }
-    return $selectItems;
-  }
+  public function submitForm(array &$form, FormStateInterface $form_state) {}
 
   /**
    * @param array $form
@@ -254,16 +213,7 @@ class BookmarkEditForm extends FormBase {
    * @return \Drupal\Core\Ajax\AjaxResponse
    */
   public function updateSession(array &$form, FormStateInterface $form_state) {
-    $response = new AjaxResponse();
-
-    $input = $form_state->getUserInput();
-    $items = self::customSelectedItems($input);
-    $_SESSION['socomec_bookmark_download'] = [];
-    if (sizeof($items)) {
-      $_SESSION['socomec_bookmark_download'] = $items;
-    }
-
-    return $response;
+    return $this->contentListFormManager->updateSession($form, $form_state, 'bookmark_action_', 'socomec_bookmark_download');
   }
 
   /**
@@ -281,49 +231,22 @@ class BookmarkEditForm extends FormBase {
    * @return \Drupal\Core\Ajax\AjaxResponse
    */
   public function removeItems(array &$form, FormStateInterface $form_state) {
-    $response = new AjaxResponse();
-    $input = $form_state->getUserInput();
-    $items = self::customSelectedItems($input);
-    if (sizeof($items)) {
-      /** @var \Drupal\soc_bookmarks\Service\Manager\soc_bookmarks $soc_bookmarks */
-      $bookmarkManager = \Drupal::service('soc_bookmarks.bookmark_manager');
-      $bookmarkManager->loadSavedItems();
-      $_SESSION['socomec_bookmark_last_deleted'] = [];
-      foreach ($items as $deletedItem) {
-        $removedLine = '#item_line_'.$deletedItem;
-        $bookmarkManager->remove($deletedItem);
-        try {
-          $bookmarkManager->updateCookie();
-          $response->addCommand(new RemoveCommand($removedLine));
-          $_SESSION['socomec_bookmark_last_deleted'][$deletedItem] = $deletedItem;
-        } catch (\Exception $e) {
-          //$this->messenger->addError($e->getMessage());
-        }
-      }
-      $count = sizeof($items);
-
-      $url = Url::fromRoute('soc_bookmarks.undo_remove_item');
-      $link = Link::fromTextAndUrl(t('Cancel deletion(s)'), $url)->toString();
-      \Drupal::messenger()->addMessage($this->t("@count item(s) deleted. @link", ["@count" => $count , "@link" => $link ]), 'status', TRUE);
-
-      $message = [
-        '#theme' => 'status_messages',
-        '#message_list' => drupal_get_messages(),
-      ];
-
-      $messages = \Drupal::service('renderer')->render($message);
-      $response->addCommand(new PrependCommand('.soc-my-list-form-message', $messages));
-    }
-
-    return $response;
+    return $this->contentListFormManager->removeItems(
+      $form,
+      $form_state,
+      'bookmark_action_',
+      'socomec_bookmark_last_deleted',
+      '.soc-my-list-form-message',
+      Url::fromRoute('soc_bookmarks.undo_remove_item'),
+      \Drupal::service('soc_bookmarks.bookmark_manager')
+      );
   }
 
   /**
    * @return array|\Drupal\Core\StringTranslation\TranslatableMarkup|mixed|null
    */
   public static function getTitle() {
-    return \Drupal::config('soc_bookmarks.settings')->get('bookmark_page_title')
-      ?? t('My documents');
+    return \Drupal::config('soc_bookmarks.settings')->get('bookmark_page_title') ?? t('My documents');
   }
 
 }
--- web/modules/custom/soc_wishlist/src/Form/WishlistEditForm.php
+++ web/modules/custom/soc_wishlist/src/Form/WishlistEditForm.php
@@ -2,22 +2,15 @@
 
 namespace Drupal\soc_wishlist\Form;
 
-use Drupal\Core\Ajax\AjaxResponse;
-use Drupal\Core\Ajax\InvokeCommand;
-use Drupal\Core\Ajax\ReplaceCommand;
-use Drupal\Core\Ajax\RemoveCommand;
-use Drupal\Core\Ajax\PrependCommand;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Messenger\MessengerInterface;
-use Drupal\Core\Render\Element\StatusMessages;
 use Drupal\node\Entity\Node;
+use Drupal\soc_content_list\Service\Manager\ContentListFormManager;
 use Drupal\soc_core\Service\MediaApi;
 use Drupal\soc_wishlist\Service\Manager\WishlistManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Url;
-use Drupal\Core\Link;
-
 
 class WishlistEditForm extends FormBase {
 
@@ -41,16 +34,19 @@ class WishlistEditForm extends FormBase {
   /**
    * WishlistController constructor.
    *
-   * @param \Drupal\soc_wishlist\Service\Manager\WishlistManager $wishlistManager
+   * @param \Drupal\soc_bookmarks\Service\Manager\BookmarkManager $bookmarkManager
    * @param \Drupal\Core\Messenger\MessengerInterface $messenger
    * @param \Drupal\soc_core\Service\MediaApi $mediaApi
+   * @param \Drupal\soc_content_list\Service\Manager\ContentListFormManager $contentListFormManager
    */
   public function __construct(WishlistManager $wishlistManager,
                               MessengerInterface $messenger,
-                              MediaApi $mediaApi) {
+                              MediaApi $mediaApi,
+                              ContentListFormManager $contentListFormManager) {
     $this->wishlistManager = $wishlistManager;
     $this->messenger = $messenger;
     $this->mediaApi = $mediaApi;
+    $this->contentListFormManager = $contentListFormManager;
   }
 
   /**
@@ -60,7 +56,8 @@ class WishlistEditForm extends FormBase {
     return new static(
       $container->get('soc_wishlist.wishlist_manager'),
       $container->get('messenger'),
-      $container->get('soc_core.media_api')
+      $container->get('soc_core.media_api'),
+      $container->get('soc_content_list.content_list_form_manager')
     );
   }
 
@@ -91,16 +88,10 @@ class WishlistEditForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $form['#theme'] = 'soc_wishlist_custom_form';
-    if (empty($this->wishlistManager)) {
-      $this->wishlistManager = \Drupal::service('soc_wishlist.wishlist_manager');
-    }
     $items = $this->wishlistManager->loadSavedItems();
 
-
-    // Form wrapper for AJAX
-    $form['wrapper_wishlist'] = [
-      '#type' => 'container',
-    ];
+    // Form wrapper_wishlist for AJAX
+    $form['wrapper_wishlist'] = ['#type' => 'container',];
 
     $form['select_all'] = [
       '#title' => 'Select all',
@@ -185,9 +176,7 @@ class WishlistEditForm extends FormBase {
       }
     }
 
-    $form['wrapper_wishlist']['nb'] = [
-      '#value' => $i
-    ];
+    $form['wrapper_wishlist']['nb'] = ['#value' => $i];
 
     $confirmRemoveClass = 'confirm-remove';
     for ($i = 0; $i < 2; ++$i) {
@@ -232,25 +221,7 @@ class WishlistEditForm extends FormBase {
       }
     }
 
-    $form['#attached']['library'][] = 'soc_content_list/list-actions';
-
-    // Confirm before deleting
-    $form['#attached']['library'][] = 'core/drupal.ajax';
-    $form['#attached']['library'][] = 'core/drupal.dialog.ajax';
-    $form['#attached']['library'][] = 'soc_content_list/ajax-confirm';
-    $form['#attached']['drupalSettings']['ajaxConfirm'][$confirmRemoveClass] = [
-      'title' => $this->t('Confirm element removal'),
-      'text' => $this->t('Are you sure that you want to remove this element?'),
-      'buttons' => [
-        'button_confirm' => ['text' => $this->t('Yes')],
-        'button_reject' => ['text' => $this->t('No')],
-      ]
-    ];
-
-    $form['#attached']['library'][] = 'soc_content_list/list-datatable';
-    $form['#attached']['drupalSettings']['socListDatatable'] = [
-      'searchPlaceholder' => $this->t('Search, filter...'),
-    ];
+    $this->contentListFormManager->attached($form, $confirmRemoveClass);
 
     return $form;
   }
@@ -265,28 +236,7 @@ class WishlistEditForm extends FormBase {
    *
    * @return array
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-  }
-
-  /**
-   * @param $input
-   *
-   * @return array
-   */
-  public static function customSelectedItems($input) {
-    $selectItems = [];
-    if (!empty($input) && is_array($input)) {
-      foreach ($input as $keyItem => $item) {
-        if (substr($keyItem, 0, 16) === "wishlist_action_") {
-          $keys = array_keys($item);
-          if (!empty($item[$keys[0]])) {
-            $selectItems[$item[$keys[0]]] = $item[$keys[0]];
-          }
-        }
-      }
-    }
-    return $selectItems;
-  }
+  public function submitForm(array &$form, FormStateInterface $form_state) {}
 
   /**
    * @param array $form
@@ -295,16 +245,21 @@ class WishlistEditForm extends FormBase {
    * @return \Drupal\Core\Ajax\AjaxResponse
    */
   public function updateSession(array &$form, FormStateInterface $form_state) {
-    $response = new AjaxResponse();
+    return $this->contentListFormManager->updateSession($form, $form_state, 'wishlist_action_', 'socomec_wishlist_export');
+  }
 
-    $input = $form_state->getUserInput();
-    $items = self::customSelectedItems($input);
-    $_SESSION['socomec_wishlist_export'] = [];
-    if (sizeof($items)) {
-      $_SESSION['socomec_wishlist_export'] = $items;
-    }
-
-    return $response;
+  /**
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *
+   * @return \Drupal\Core\Ajax\AjaxResponse
+   */
+  public function removeItems(array &$form, FormStateInterface $form_state) {
+    return $this->contentListFormManager->removeItems($form, $form_state,
+      'wishlist_action_','socomec_wishlist_last_deleted','.soc-my-list-form-message',
+      Url::fromRoute('soc_wishlist.undo_remove_item'),
+      \Drupal::service('soc_wishlist.wishlist_manager')
+    );
   }
 
   /**
@@ -337,57 +292,14 @@ class WishlistEditForm extends FormBase {
     }
   }
 
-  /**
-   * @param array $form
-   * @param \Drupal\Core\Form\FormStateInterface $form_state
-   *
-   * @return \Drupal\Core\Ajax\AjaxResponse
-   */
-  public function removeItems(array &$form, FormStateInterface $form_state) {
-    $response = new AjaxResponse();
-    $input = $form_state->getUserInput();
-    $items = self::customSelectedItems($input);
-    if (sizeof($items)) {
-      /** @var \Drupal\soc_wishlist\Service\Manager\WishlistManager $wishlistManager */
-      $wishlistManager = \Drupal::service('soc_wishlist.wishlist_manager');
-      $wishlistManager->loadSavedItems();
-      $_SESSION['socomec_wishlist_last_deleted'] = [];
-      foreach ($items as $deletedItem) {
-        $removedLine = '#item_line_'.$deletedItem;
-        $wishlistManager->remove($deletedItem);
-        try {
-          $wishlistManager->updateCookie();
-          $response->addCommand(new RemoveCommand($removedLine));
-          $_SESSION['socomec_wishlist_last_deleted'][$deletedItem] = $deletedItem;
-        } catch (\Exception $e) {
-          //$this->messenger->addError($e->getMessage());
-        }
-      }
-      $count = sizeof($items);
 
-      $url = Url::fromRoute('soc_wishlist.undo_remove_item');
-      $link = Link::fromTextAndUrl(t('Cancel deletion(s)'), $url)->toString();
-      \Drupal::messenger()->addMessage($this->t("@count item(s) deleted. @link", ["@count" => $count , "@link" => $link ]), 'status', TRUE);
-
-      $message = [
-        '#theme' => 'status_messages',
-        '#message_list' => drupal_get_messages(),
-      ];
-
-      $messages = \Drupal::service('renderer')->render($message);
-      $response->addCommand(new PrependCommand('.soc-my-list-form-message', $messages));
-    }
-
-    return $response;
-  }
 
   /**
    * @return array|\Drupal\Core\StringTranslation\TranslatableMarkup|mixed|null
    */
   public static function getTitle() {
     $language = \Drupal::languageManager()->getCurrentLanguage();
-    return \Drupal::config('soc_wishlist.settings')->get('page_title_' . $language->getId())
-      ?? t('My wishlist');
+    return \Drupal::config('soc_wishlist.settings')->get('page_title_' . $language->getId()) ?? t('My wishlist');
   }
 
 }
