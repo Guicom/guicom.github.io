3c63d7fe06d4e43eddbba352e4647f3c1de1887e
Guillaume <guicom.actency@gmail.com>
2020-05-19 11:12:30 +0200

    #1338 - Add rollback service

---
+++ web/modules/custom/soc_nextpage/src/Service/RollbackImport.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\soc_nextpage\Service;
+
+class RollbackImport {
+
+  public function writeJob() {
+
+  }
+
+  public function markAsUpdate() {
+
+  }
+
+  public function rollBack() {
+
+  }
+
+  public function rollBackNode() {
+
+  }
+
+  public function rollBackfamily() {
+
+  }
+
+}
