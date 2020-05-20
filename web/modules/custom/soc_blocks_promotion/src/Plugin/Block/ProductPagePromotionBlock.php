46e3a11bba6a2a23839d5381bc514b6424dac0d3
cklein <christophe.klein@actency.fr>
2020-05-20 14:19:50 +0200

    SOCSOB-1332 Display product page promo block.

---
+++ web/modules/custom/soc_blocks_promotion/src/Plugin/Block/ProductPagePromotionBlock.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Drupal\soc_blocks_promotion\Plugin\Block;
+
+use Drupal\Core\Cache\Cache;
+use Drupal\paragraphs\Entity\Paragraph;
+use Drupal\soc_blocks_promotion\Entity\BlockPromotionEntity;
+
+/**
+ * Provides a block that displays the a promo block entity.
+ *
+ * @Block(
+ *   id = "promotion_block_product_page",
+ *   admin_label = @Translation("Product page Promotion block")
+ * )
+ */
+class ProductPagePromotionBlock extends PromotionBlock {
+
+  public function build() {
+    $build = [];
+    $build['node_id'] = [
+      '#type' => 'inline_template',
+      '#template' => $this->getContent() ?? '',
+    ];
+    return $build;
+  }
+
+  /**
+   * Get the promo block as rendered HTML.
+   *
+   * @return string|null
+   */
+  public function getContent(): ?string {
+    // Get current node.
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $field = $node->get('field_promo_block_paragraph');
+    $fieldValue = $field->getValue();
+    if (sizeof($fieldValue)) {
+      $paragraphId = $fieldValue[0]['target_id'];
+      // Get promo block paragraph.
+      if ($paragraph = Paragraph::load($paragraphId)) {
+        $field = $paragraph->get('field_promo_block');
+        $fieldValue = $field->getValue();
+        if (sizeof($fieldValue)) {
+          // Select block to display.
+          $promoBlockIds = [];
+          foreach ($fieldValue as $value) {
+            $promoBlockIds[] = $value['target_id'];
+          }
+          $promoBlocks = BlockPromotionEntity::loadMultiple($promoBlockIds);
+          // By default, pick last block.
+          $lastBlock = end($promoBlocks);
+          $blockToDisplay = $lastBlock;
+          // But if last block is not published, then pick first block.
+          $firstBlock = reset($promoBlocks);
+          if ($lastBlock->get('moderation_state')->getString() !== 'published') {
+            $blockToDisplay = $firstBlock;
+          }
+          // Get HTML render for the chosen block.
+          $view_builder = \Drupal::entityTypeManager()->getViewBuilder('block_promotion_entity');
+          $pre_render = $view_builder->view($blockToDisplay);
+          return render($pre_render);
+        }
+      }
+    }
+    return '';
+  }
+
+  public function getCacheTags() {
+    if ($node = \Drupal::routeMatch()->getParameter('node')) {
+      return Cache::mergeTags(parent::getCacheTags(), array('node:' . $node->id()));
+    } else {
+      return parent::getCacheTags();
+    }
+  }
+
+  public function getCacheContexts() {
+    return Cache::mergeContexts(parent::getCacheContexts(), array('route'));
+  }
+
+}
---
+++ web/modules/custom/soc_blocks_promotion/src/Plugin/Block/PromotionBlock.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Drupal\soc_blocks_promotion\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+abstract class PromotionBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   *
+   * @return static
+   */
+  public static function create(ContainerInterface $container,
+                                array $configuration,
+                                $plugin_id,
+                                $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition
+    );
+  }
+
+  public function build() {
+    $build = [];
+    return $build;
+  }
+
+}
