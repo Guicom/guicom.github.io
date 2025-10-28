6c0b6404c686064913c621da67c9e7a4891c0f38
Florian Wust <florian.wust1@gmail.com>
2025-10-28 15:28:00 +0100

    pym.js : send height on form submit

--- pym-init.js
+++ pym-init.js
@@ -1,8 +1,10 @@
 (function() {
     var pymChild = new pym.Child();
     window.addEventListener('load', function() {
+        console.log("[PYM] Loaded")
         setTimeout(function() {
             pymChild.sendHeight();
+            console.log("[PYM] Height sent")
         }, 300);
     });
 })();
